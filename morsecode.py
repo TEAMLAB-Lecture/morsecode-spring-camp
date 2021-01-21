0712283a17ef5b0c3a871765d7d76f13d07c1f82
배새봄 <2bee2@naver.com>
2021-01-21 13:20:11 +0900

    modify is_help_command & set main function

--- morsecode.py
+++ morsecode.py
@@ -51,8 +51,9 @@ def is_help_command(user_input):
     """
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당 또는 필요에 따라 자유로운 수정
-    result = None
-
+    result = False
+    if user_input.lower() == 'h' or user_input.lower() == 'help':
+        result = True
     return result
     # ==================================
 
@@ -260,8 +261,19 @@ def encoding_sentence(english_sentence):
 def main():
     print("Morse Code Program!!")
     # ===Modify codes below=============
-
-
+    input_string = input("Input your message(H - Help, 0 - Exit: ")
+    while True:
+        if input_string == '0':
+            break
+        if is_help_command(input_string):
+            print(get_help_message())
+        elif is_validated_english_sentence(input_string):
+            print(encoding_sentence(input_string))
+        elif is_validated_morse_code(input_string):
+            print(decoding_sentence(input_string))
+        else:
+            print("Wrong Input")
+        input_string = input("Input your message(H - Help, 0 - Exit: ")
 
     # ==================================
     print("Good Bye")
