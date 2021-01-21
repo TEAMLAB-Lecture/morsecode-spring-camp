6dc11e6ffe7574822edccb9733fa1dc03d6fdcd1
배새봄 <2bee2@naver.com>
2021-01-21 13:49:34 +0900

    modify get_cleaned_english_sentence

--- morsecode.py
+++ morsecode.py
@@ -153,8 +153,7 @@ def get_cleaned_english_sentence(raw_english_sentence):
     """
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당 또는 필요에 따라 자유로운 수정
-    result = None
-
+    result = re.sub(r"[\.|,|\?|!]+", "", raw_english_sentence)
     return result
     # ==================================
 
